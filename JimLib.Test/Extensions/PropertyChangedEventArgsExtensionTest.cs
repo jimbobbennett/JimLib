099d380b4c57106ba16a73d9dad5d648f6773f53
Jim Bennett <jimbobbennett@mac.com>
2014-09-06 12:13:19 +0700

    Added extension to match property names

---
+++ JimLib.Test/Extensions/PropertyChangedEventArgsExtensionTest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using FluentAssertions;
+using JimBobBennett.JimLib.Extensions;
+using NUnit.Framework;
+
+namespace JimBobBennett.JimLib.Test.Extensions
+{
+    [TestFixture]
+    public class PropertyChangedEventArgsExtensionTest
+    {
+        private bool Foo { get; set; }
+
+        [TestCase("Foo", true)]
+        [TestCase("", true)]
+        [TestCase("Bar", false)]
+        public void PropertyNameMatches(string propertyName, bool expected)
+        {
+            new PropertyChangedEventArgs(propertyName).PropertyNameMatches(() => Foo).Should().Be(expected);
+        }
+    }
+}
---
+++ JimLib/Extensions/PropertyChangedEventArgsExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq.Expressions;
+
+namespace JimBobBennett.JimLib.Extensions
+{
+    public static class PropertyChangedEventArgsExtension
+    {
+        /// <summary>
+        /// Gets if a property change matches the given property name.
+        /// This will also match any property to string.Empty as this is the standard way
+        /// to indicate all properties have changed
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="args"></param>
+        /// <param name="propertyExpression"></param>
+        /// <returns></returns>
+        [Pure]
+        public static bool PropertyNameMatches<TValue>(this PropertyChangedEventArgs args, Expression<Func<TValue>> propertyExpression)
+        {
+            return args.PropertyName == string.Empty || args.PropertyName == args.ExtractPropertyName(propertyExpression);
+        }
+    }
+}
